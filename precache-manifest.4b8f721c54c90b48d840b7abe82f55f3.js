b09a4fe167be55b55093cd96c09f0fc1032430f3
Your Name <youremail@domain.com>
2019-01-10 14:29:15 +0200

    Updates

---
+++ precache-manifest.4b8f721c54c90b48d840b7abe82f55f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c7edb80cf545303f47a",
+    "url": "/react-simpleshopcart/static/js/runtime~main.1c7edb80.js"
+  },
+  {
+    "revision": "f7dc2e360e74d79baef4",
+    "url": "/react-simpleshopcart/static/js/main.f7dc2e36.chunk.js"
+  },
+  {
+    "revision": "48c02728f9744e3602da",
+    "url": "/react-simpleshopcart/static/js/1.48c02728.chunk.js"
+  },
+  {
+    "revision": "f7dc2e360e74d79baef4",
+    "url": "/react-simpleshopcart/static/css/main.a867ef39.chunk.css"
+  },
+  {
+    "revision": "48c02728f9744e3602da",
+    "url": "/react-simpleshopcart/static/css/1.194d2ef2.chunk.css"
+  },
+  {
+    "revision": "56aba7a33474bff9f7baa92be5d588ce",
+    "url": "/react-simpleshopcart/index.html"
+  }
+];
